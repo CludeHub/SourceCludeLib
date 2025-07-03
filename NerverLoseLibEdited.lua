23f13b37ce83a95762393d31259245e886692551
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-07-03 18:27:29 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -597,7 +597,7 @@ updateDPI()
 		ImageButtons.BackgroundTransparency = 1.000
 		ImageButtons.BorderColor3 = Color3.fromRGB(0, 0, 0)
 		ImageButtons.BorderSizePixel = 0
-		ImageButtons.Position = UDim2.new(0.908723712, 0, 0.0239103697, 0)
+		ImageButtons.Position = UDim2.new(0.908723712, 0, 0.0139103697, 0)
 		ImageButtons.Size = UDim2.new(0.0700000036, 0, 0.0700000036, 0)
 		ImageButtons.SizeConstraint = Enum.SizeConstraint.RelativeYY
 		ImageButtons.ZIndex = 4
