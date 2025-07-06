042a67d9d97c26a1584188fe6d1a19e1d2193ac7
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-07-06 22:14:07 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -616,6 +616,34 @@ end)
 -- Initial DPI set
 updateDPI()
 
+			local ImageButtons2 = Instance.new("ImageButton")
+
+		ImageButtons2.Parent = Frame
+		ImageButtons2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		ImageButtons2.BackgroundTransparency = 1.000
+		ImageButtons2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+		ImageButtons2.BorderSizePixel = 0
+		ImageButtons2.Position = UDim2.new(0.87670207, 0, 0.0139103699, 0)
+		ImageButtons2.Size = UDim2.new(0.0600000036, 0, 0.0600000036, 0)
+		ImageButtons2.SizeConstraint = Enum.SizeConstraint.RelativeYY
+		ImageButtons2.ZIndex = 4
+		ImageButtons2.Image = "rbxassetid://134488580093972"
+		ImageButtons2.ScaleType = Enum.ScaleType.Fit
+
+		ImageButtons2.MouseButton1Click:Connect(function()
+			local Sound2 = Instance.new("Sound")
+Sound2.SoundId = "rbxassetid://6895079853"
+Sound2.Looped = false
+Sound2.Volume = 1
+Sound2.Parent = game:GetService("SoundService")
+Sound2:Play()
+
+Sound2.Ended:Connect(function()
+	Sound2:Destroy()
+     end)
+			
+		end)
+
 		local ImageButtons = Instance.new("ImageButton")
 
 		ImageButtons.Parent = Frame
@@ -624,7 +652,7 @@ updateDPI()
 		ImageButtons.BorderColor3 = Color3.fromRGB(0, 0, 0)
 		ImageButtons.BorderSizePixel = 0
 		ImageButtons.Position = UDim2.new(0.918723712, 0, 0.0139103697, 0)
-		ImageButtons.Size = UDim2.new(0.0700000036, 0, 0.0700000036, 0)
+		ImageButtons.Size = UDim2.new(0.0600000036, 0, 0.0600000036, 0)
 		ImageButtons.SizeConstraint = Enum.SizeConstraint.RelativeYY
 		ImageButtons.ZIndex = 4
 		ImageButtons.Image = "rbxassetid://134488580093972"
