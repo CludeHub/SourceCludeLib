3ddfb85d37b790877b13b1a241feeb6738798661
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-08-25 17:37:25 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -1694,7 +1694,8 @@ Sound2.Ended:Connect(function()
 				Icon.Name = "Icon"
 				Icon.Parent = Effect
 				Icon.AnchorPoint = Vector2.new(0.5, 0.5)
-				Icon.BackgroundColor3 = Color3.fromRGB(95, 95, 95)
+				Icon.BackgroundColor3 = Color3.fromRGB(223, 251, 255)
+				Icon.BackgroundTransparency = 0.300
 				Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
 				Icon.BorderSizePixel = 0
 				Icon.Position = UDim2.new(0.75, 0, 0.5, 0)
@@ -1712,7 +1713,7 @@ Sound2.Ended:Connect(function()
 						Effect.BackgroundColor3 = Color3.fromRGB(12, 18, 50)
 					else
 						TweenService:Create(LabelText,TweenInfo.new(timea),{TextTransparency=0.3}):Play()
-						TweenService:Create(Icon,TweenInfo.new(timea),{Position=UDim2.new(0.25,0,0.5,0),BackgroundColor3=Color3.fromRGB(95, 95, 95)}):Play();
+						TweenService:Create(Icon,TweenInfo.new(timea),{Position=UDim2.new(0.25,0,0.5,0),BackgroundColor3=Color3.fromRGB(223, 251, 255)}):Play();
 						Effect.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
 					end
 				end
