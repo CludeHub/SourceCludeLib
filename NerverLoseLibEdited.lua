e558fd43fd229aa71b0ba244300c92fe5a23d7a1
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-08-26 14:28:44 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -1597,7 +1597,7 @@ Sound2.Ended:Connect(function()
 				LabelText.BorderColor3 = Color3.fromRGB(0, 0, 0)
 				LabelText.BorderSizePixel = 0
 				LabelText.Position = UDim2.new(0.5, 0, 0.5, 0)
-				LabelText.Size = UDim2.new(1, -2, 0.649999976, 0)
+				LabelText.Size = UDim2.new(1, -2, 0.649999976, 10)
 				LabelText.ZIndex = 6
 				LabelText.Font = Enum.Font.SourceSansSemibold
 				LabelText.Text = LabelNameString or"Label"
