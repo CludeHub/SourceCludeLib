f6ad3738eb118cab2bb4d9c55f8191b947aaa901
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-12-10 15:45:36 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -452,7 +452,7 @@ print(Text .. " is Running")
 	Frame.Parent = ScreenGui
 	Frame.Active = true
 	Frame.BackgroundColor3 = NEVERLOSE.Themes.BlackgroundColor
-	Frame.BackgroundTransparency = 0.085
+	Frame.BackgroundTransparency = 0.2
 	Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	Frame.BorderSizePixel = 0
 	Frame.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -1532,7 +1532,7 @@ Sound2.Ended:Connect(function()
 
 		function TabFunctions:AddSection(SectionName,parentname)
 			parentname = parentname or "left"
-
+            local SectionHeader = Instance.new("Frame")
 			local Section = Instance.new("Frame")
 			local UICorner = Instance.new("UICorner")
 			local UIStroke = Instance.new("UIStroke")
@@ -1540,13 +1540,22 @@ Sound2.Ended:Connect(function()
 			local UIListLayout = Instance.new("UIListLayout")
 			local lay = Instance.new("Frame")
 			local UICorner_2 = Instance.new("UICorner")
-
+local Sectionh = SectionHeader
+			Sectionh.Name = "SectionHeader"
+			Sectionh.Parent = (parentname=="left"and Left) or (parentname=="right"and Right) or nil
+			Sectionh.BackgroundTransparency = 1
+			Sectionh.BorderColor3 = Color3.fromRGB(0, 0, 0)
+			Sectionh.BorderSizePixel = 1
+			Sectionh.Size = UDim2.new(0, 0, 0, 0)
+			Sectionh.AutomaticSize = Enum.AutomaticSize.Y
+			Sectionh.ZIndex = 4
+			
 			Section.Name = "Section"
-			Section.Parent = (parentname=="left"and Left) or (parentname=="right"and Right) or nil
-
+			Section.Parent = Sectionh
 			Section.BackgroundColor3 = NEVERLOSE.Themes.SectionColor
 			Section.BorderColor3 = Color3.fromRGB(0, 0, 0)
 			Section.BorderSizePixel = 0
+			Section.Postion = UDim2.new(0, 0, 0.05, 0)
 			Section.Size = UDim2.new(0.970000029, 0, 0, 0)
 			Section.ZIndex = 5
 
@@ -1554,24 +1563,24 @@ Sound2.Ended:Connect(function()
 			UICorner.Parent = Section
 
 			Header.Name = "Header"
-			Header.Parent = Section
+			Header.Parent = Sectionh
 			Header.AnchorPoint = Vector2.new(0.5, 0)
 			Header.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 			Header.BackgroundTransparency = 1.000
 			Header.BorderColor3 = Color3.fromRGB(0, 0, 0)
 			Header.BorderSizePixel = 0
-			Header.Position = UDim2.new(0.5, 0, 0.100000001, 0)
+			Header.Position = UDim2.new(0.5, 0, 0, 0)
 			Header.Size = UDim2.new(1, 0, 0.1, 0)
 			Header.ZIndex = 5
 			Header.Font = Enum.Font.ArialBold
 			Header.Text = SectionName or "Section"
-			Header.TextColor3 = Color3.fromRGB(255, 255, 255)
+			Header.TextColor3 = Color3.fromRGB(200,200,228)
 			Header.TextScaled = true
 			Header.TextSize = 8.000
 			Header.TextWrapped = true
 			Header.TextXAlignment = Enum.TextXAlignment.Left
 			Header.TextTransparency=1
-			TweenService:Create(Header,TweenInfo.new(0.3),{TextTransparency=0}):Play()
+			TweenService:Create(Header,TweenInfo.new(0.3),{TextTransparency=0.3}):Play()
 
 			local AspectRatio = Instance.new("UIAspectRatioConstraint")
 
@@ -1593,10 +1602,11 @@ Sound2.Ended:Connect(function()
 			lay.Size = UDim2.new(0.970000029, 0, 0, 2)
 			lay.ZIndex = 5
 			lay.BackgroundTransparency=1
+			
 			Section.BackgroundTransparency=1
 			UIStroke.Transparency=1
 
-			TweenService:Create(lay,TweenInfo.new(1),{BackgroundTransparency=0}):Play()
+			TweenService:Create(lay,TweenInfo.new(1),{BackgroundTransparency=1}):Play()
 			TweenService:Create(Section,TweenInfo.new(1),{BackgroundTransparency=0}):Play()
 			TweenService:Create(UIStroke,TweenInfo.new(1),{Transparency=0}):Play()
 
