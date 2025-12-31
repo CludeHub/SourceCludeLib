0f6e846a310be862258ddaf08152b94c5906406b
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-12-31 19:22:32 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -1909,7 +1909,7 @@ Line.Parent = Toggle
 				Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
 				Icon.BorderSizePixel = 0
 				Icon.Position = UDim2.new(0.75, 0, 0.5, 0)
-				Icon.Size = UDim2.new(1.27, 0, 1.21, 0)
+				Icon.Size = UDim2.new(1.15, 0, 1.15, 0)
 				Icon.SizeConstraint = Enum.SizeConstraint.RelativeYY
 				Icon.ZIndex = 7
 
