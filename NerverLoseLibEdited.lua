6c2703dfe7a095c7dfd8a1adcbdb22d9334e8cc9
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-12-31 18:28:48 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -1855,11 +1855,11 @@ Sound2.Ended:Connect(function()
 				
 local Line = Instance.new('Frame')
 Line.Position = UDim2.new(0.05000000029802322,0,1,0)
-Line.Size = UDim2.new(0.8900000047683716,0,0.003000000026077032,0)
+Line.Size = UDim2.new(0.8900000047683716,0,0,1)
 Line.BorderSizePixel = 0
 Line.AnchorPoint = Vector2.new(0,0)
 Line.BackgroundColor3 = Color3.fromRGB(3,35,50)
-Line.BackgroundTransparency = 0.6
+Line.BackgroundTransparency = 0
 Line.ZIndex = 10
 Line.Parent = Toggle
 
@@ -2016,10 +2016,10 @@ Sound2.Ended:Connect(function()
 
 				local Line = Instance.new('Frame')
 Line.Position = UDim2.new(0.07000000029802322,0,1,0)
-Line.Size = UDim2.new(0.8700000047683716,0,0.003000000026077032,0)
+Line.Size = UDim2.new(0.8700000047683716,0,0,1)
 Line.AnchorPoint = Vector2.new(0,0)
 Line.BackgroundColor3 = Color3.fromRGB(3,35,50)
-Line.BackgroundTransparency = 0.6
+Line.BackgroundTransparency = 0
 Line.BorderSizePixel = 0
 Line.ZIndex = 10
 Line.Parent = Keybind
@@ -2188,10 +2188,10 @@ Sound2.Ended:Connect(function()
 
 				local Line = Instance.new('Frame')
 Line.Position = UDim2.new(0.05000000029802322,0,1,0)
-Line.Size = UDim2.new(0.8900000047683716,0,0.003000000026077032,0)
+Line.Size = UDim2.new(0.8900000047683716,0,0,1)
 Line.AnchorPoint = Vector2.new(0,0)
 Line.BackgroundColor3 = Color3.fromRGB(3,35,50)
-Line.BackgroundTransparency = 0.6
+Line.BackgroundTransparency = 0
 Line.BorderSizePixel = 0
 Line.ZIndex = 10
 Line.Parent = Slider
@@ -2395,10 +2395,10 @@ Line.Parent = Slider
 
 				local Line = Instance.new('Frame')
 Line.Position = UDim2.new(0.05000000029802322,0,1,0)
-Line.Size = UDim2.new(0.8900000047683716,0,0.003000000026077032,0)
+Line.Size = UDim2.new(0.8900000047683716,0,0,1)
 Line.AnchorPoint = Vector2.new(0,0)
 Line.BackgroundColor3 = Color3.fromRGB(3,35,50)
-Line.BackgroundTransparency = 0.6
+Line.BackgroundTransparency = 0
 Line.BorderSizePixel = 0
 Line.ZIndex = 10
 Line.Parent = Dropdown
